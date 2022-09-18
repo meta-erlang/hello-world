749fe678819b4f7fc0764374985062e095bae33e
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2022-09-18 18:17:23 -0300

    Get rust target from environment variable

--- hello-elixir-rustler/lib/hello_elixir_rustler/rustler_nif.ex
+++ hello-elixir-rustler/lib/hello_elixir_rustler/rustler_nif.ex
@@ -1,7 +1,8 @@
 defmodule HelloElixirRustler.RustlerNif do
   use Rustler,
     otp_app: :hello_elixir_rustler,
-    crate: "helloelixirrustler_rustlernif"
+    crate: "helloelixirrustler_rustlernif",
+    target: System.get_env("RUST_TARGET")
 
   # When your NIF is loaded, it will override this function.
   def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
